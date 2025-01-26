5b3d6ea072509b4fdd74d1dcbe2b6241b94056d6
dizlexic <dan@mooresolutions.io>
2025-01-25 21:24:50 -0600

    Update email closing salutation in candidate questionnaire
    Replaced "Best regards" with "Thanks" for a more casual and approachable tone in the candidate questionnaire email template. No functional changes made beyond the text update.

--- resources/views/mail/candidate-questionnaire.blade.php
+++ resources/views/mail/candidate-questionnaire.blade.php
@@ -4,7 +4,7 @@
     <p>
         <a href="{{ route('questionnaire.create', ['candidate_id' => $candidate->id]) }}">
             {{ route('questionnaire.create', ['candidate_id' => $candidate->id]) }}        </a></p>
-    <p>Best regards,</p>
+    <p>Thanks,</p>
     <p>{{ config('app.name') }}</p>
     <p><a href="{{ route('homepage') }}">{{ route('homepage') }}</a></p>
 </div>
