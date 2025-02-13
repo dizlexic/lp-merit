03e92348d4a98a0bf49bdc52b2bc8e3f740b6852
Daniel Moore <dan@mooresolutions.io>
2025-02-12 21:02:49 -0600

    Refactor UI components for pagination and search results.
    Updated `Pagination` component to optimize markup and handle "Previous" and "Next" links with icons. Enhanced `HomeView` and `SearchView` components with better structure, improved slide functionality, and skeleton loaders for image placeholders.

--- database/seeders/DevelopmentSeeder.php
+++ database/seeders/DevelopmentSeeder.php
@@ -3,12 +3,21 @@
 namespace Database\Seeders;
 
 use App\Models\Candidate;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DevelopmentSeeder extends Seeder
 {
     public function run(): void
     {
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@test.ic',
+            'password' => Hash::make('password'),
+
+        ])->assignRole('admin');
+
         $this->call([
             CandidateSeeder::class
         ]);
