36f8fa4164c7c5a33fb250dedec658e41876a050
Daniel Moore <dan@mooresolutions.io>
2025-02-15 19:47:10 -0600

    Refactor Vue components to use `$page.props` for props.
    Updated `DetailView.vue` and `QuestionnaireCreate.vue` to replace the `defineProps` API by directly accessing data from `$page.props`. This simplifies the code base and improves consistency with other parts of the application.

--- resources/js/Pages/DetailView.vue
+++ resources/js/Pages/DetailView.vue
@@ -1,33 +1,28 @@
 <script setup>
-
 import ApplicationLayout from '@/layouts/ApplicationLayout.vue';
-import { defineProps } from 'vue';
-
-const props = defineProps({
-  candidate: Object,
-});
 </script>
 
 <template>
-  <application-layout>
-    <v-row>
-      <v-col cols="12">
-        <v-card>
-          <v-card-title>{{ candidate.name }}</v-card-title>
-          <v-card-subtitle>{{ candidate.state }}</v-card-subtitle>
-          <v-card-item>
-            <v-img
-              :width="300"
-              aspect-ratio="16/9"
-              cover
-              :src="candidate.picture" />
-          </v-card-item>
-        </v-card>
-      </v-col>
-    </v-row>
-  </application-layout>
+    <application-layout>
+        <v-row>
+            <v-col cols="12">
+                <v-card>
+                    <v-card-title>{{
+                        $page.props.candidate.name
+                    }}</v-card-title>
+                    <v-card-subtitle>{{
+                        $page.props.candidate.state
+                    }}</v-card-subtitle>
+                    <v-card-item>
+                        <v-img
+                            :width="300"
+                            aspect-ratio="16/9"
+                            cover
+                            :src="$page.props.candidate.picture"
+                        />
+                    </v-card-item>
+                </v-card>
+            </v-col>
+        </v-row>
+    </application-layout>
 </template>
-
-<style scoped>
-
-</style>
--- resources/js/Pages/QuestionnaireCreate.vue
+++ resources/js/Pages/QuestionnaireCreate.vue
@@ -2,28 +2,26 @@
 import ApplicationLayout from '@/layouts/ApplicationLayout.vue';
 
 defineProps({
-  candidate: Object,
-  questions: Array,
-})
+    candidate: Object,
+    questions: Array,
+});
 </script>
 
 <template>
-  <application-layout>
-    <v-card v-for="question in questions">
-      <v-card-title>
-        {{ question.text }}
-      </v-card-title>
-      <v-card-text>
-        <v-text-field
-          v-model="question.answer"
-          label="Answer"
-          type="text"
-        />
-      </v-card-text>
-    </v-card>
-  </application-layout>
+    <application-layout>
+        <v-card v-for="question in questions">
+            <v-card-title>
+                {{ question.text }}
+            </v-card-title>
+            <v-card-text>
+                <v-text-field
+                    v-model="question.answer"
+                    label="Answer"
+                    type="text"
+                />
+            </v-card-text>
+        </v-card>
+    </application-layout>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
