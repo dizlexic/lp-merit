52d3a9eb13331cabea87f32d7fa7db97aa2b5fb7
Daniel Moore <dan@mooresolutions.io>
2025-02-15 20:10:49 -0600

    Add basic theme support
    Introduced an `AppLightTheme` with customized colors and integrated it into the Vuetify configuration. Refactored the existing Vuetify initialization to include theme support and ensure proper structure.

--- resources/js/plugins/vuetify.js
+++ resources/js/plugins/vuetify.js
@@ -1,17 +1,26 @@
+import '@mdi/font/css/materialdesignicons.css'; // Ensure you are using css-loader
+import 'vuetify/styles';
 // Vuetify
-import 'vuetify/styles'
-import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
+import { AppLightTheme } from '@/themes/AppLightTheme';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+
+const theme = {
+    defaultTheme: 'AppLightTheme',
+    themes: {
+        AppLightTheme,
+    },
+};
 
 const conf = {
-  components,
-  directives,
-}
-export const vuetify = createVuetify(conf)
+    components,
+    directives,
+    theme,
+};
+export const vuetify = createVuetify(conf);
 
 export const ssr = createVuetify({
-  ...conf,
-  ssr: true,
-})
+    ...conf,
+    ssr: true,
+});
